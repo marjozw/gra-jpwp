45256476017e3c653f27ef47e3371269ba9046da
marjozw <45153135+marjozw@users.noreply.github.com>
2018-12-20 12:19:23 +0100

    Add files via upload

---
+++ GraMatematycznyDeszcz/GraMatematycznyDeszcz/Form1.cs
@@ -0,0 +1,191 @@
+﻿#define My_Debug
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraMatematycznyDeszcz
+{
+    public partial class GameForm : Form
+    {
+        int cursX = 0;
+        int cursY = 0;
+        int speed = 15;
+        int wynik = 0;
+        int timeTick = 0;
+        bool begin = true;
+        bool begin2 = true;
+        bool begin3 = true;
+        Random rand = new Random();
+        PictureBox raindrop = new PictureBox();
+        
+        public GameForm()
+        {
+            InitializeComponent();
+            reset();
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams handleParam = base.CreateParams;
+                handleParam.ExStyle |= 0x02000000;   // WS_EX_COMPOSITED       
+                return handleParam;
+            }
+        }
+    
+      
+      
+        private void timerLoop_Tick(object sender, EventArgs e)
+        {
+            score.Text = "Wynik " + wynik;
+            raindropFall();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics dc = e.Graphics;
+#if My_Debug
+            TextFormatFlags flags = TextFormatFlags.HorizontalCenter | TextFormatFlags.EndEllipsis;
+            Font font = new Font("Stencil",12,FontStyle.Regular);
+            TextRenderer.DrawText(dc,"X=" + cursX.ToString() + ":" + "Y=" + cursY.ToString(),font, new Rectangle(0,0,120,20), SystemColors.ControlText, flags);
+#endif
+        //    kropla.DrawImage(dc);
+            base.OnPaint(e);
+        }
+        
+        private void GameForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            cursX = e.X;
+            cursY = e.Y;
+
+            this.Refresh();
+        }
+
+        private void reset()
+        {
+                speed = 15;
+                timerLoop.Start();
+        }
+        private void raindropFall()
+        {
+            
+            /*   int random_start = rand.Next(1, 3);
+               switch (random_start)
+               {
+                   case 1: kropla1.fallDown(go1, speed, kropla1.Location.X); break;
+                   case 2: kropla2.fallDown(go2, speed, kropla1.Location.X); break;
+                   case 3: kropla3.fallDown(go3, speed, kropla1.Location.X); break;
+
+               }*/
+            if (begin)
+            {
+                kropla1.fallDown(speed);
+                begin = false;
+            }
+           //ruch kropli w kolumnie pierwszej
+           if(kropla4.Visible == false && kropla1.Top > 140)
+            {
+                kropla4.fallDown(speed);
+            }
+            else if (kropla4.Visible == true)
+            {
+                kropla4.fallDown(speed);
+            }
+            if(kropla1.Visible == false && kropla4.Top > 140)
+            {
+                kropla1.fallDown(speed);
+            }
+            else if (kropla1.Visible == true)
+            {
+                kropla1.fallDown(speed);
+            }
+            if(kropla1.Top > 400)
+            {
+                kropla1.resetRaindrop(kropla1.Location.X);
+            }
+            if(kropla4.Top > 400)
+            {
+                kropla4.resetRaindrop(kropla4.Location.X);
+            }
+            timeTick += 500;
+            //ruch kropli w kolumnie drugiej
+            if(timeTick > 4000 && begin2)
+            {
+                kropla2.fallDown(speed);
+                begin2 = false;
+            }
+            if (kropla2.Visible == false && kropla5.Top > 140)
+            {
+                kropla2.fallDown(speed);
+            }
+            else if (kropla2.Visible == true)
+            {
+                kropla2.fallDown(speed);
+            }
+            if (kropla5.Visible == false && kropla2.Top > 140)
+            {
+                kropla5.fallDown(speed);
+            }
+            else if (kropla5.Visible == true)
+            {
+                kropla5.fallDown(speed);
+            }
+            if (kropla5.Top > 400)
+            {
+                kropla5.resetRaindrop(kropla5.Location.X);
+            }
+            if (kropla2.Top > 400)
+            {
+                kropla2.resetRaindrop(kropla2.Location.X);
+            }
+            //ruch kropli w kolumnie trzeciej
+            if (timeTick > 7000 && begin3)
+            {
+                kropla3.fallDown(speed);
+                begin3 = false;
+            }
+            if (kropla3.Visible == false && kropla6.Top > 140)
+            {
+                kropla3.fallDown(speed);
+            }
+            else if (kropla3.Visible == true)
+            {
+                kropla3.fallDown(speed);
+            }
+            if (kropla6.Visible == false && kropla3.Top > 140)
+            {
+                kropla6.fallDown(speed);
+            }
+            else if (kropla6.Visible == true)
+            {
+                kropla6.fallDown(speed);
+            }
+            if (kropla6.Top > 400)
+            {
+                kropla6.resetRaindrop(kropla6.Location.X);
+            }
+            if (kropla3.Top > 400)
+            {
+                kropla3.resetRaindrop(kropla3.Location.X);
+            }
+
+
+
+            /*     if (timeTick > 4000)
+                 {
+                     kropla2.fallDown(speed);
+                     if (kropla2.Top > 250)
+                     {
+                         kropla5.fallDown(speed);
+                     }
+                 }
+              */
+        }
+    }
+}
---
+++ GraMatematycznyDeszcz/GraMatematycznyDeszcz/Kropla.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using GraMatematycznyDeszcz.Properties;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GraMatematycznyDeszcz
+{
+   public class Kropla : Panel 
+    {
+        
+        private void CustomPanel_Resize(object sender, EventArgs e)
+        {
+            if (this.Visible) this.Refresh();
+
+        }
+        public Kropla()
+        {
+            // this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+            this.Resize += new EventHandler(CustomPanel_Resize);
+            this.SetStyle(ControlStyles.UserPaint |
+                          ControlStyles.AllPaintingInWmPaint |
+                          ControlStyles.OptimizedDoubleBuffer,
+                          true);
+        }
+        public void fallDown(int speed)
+        {
+                this.Visible = true;
+                this.Top += speed;
+        }
+        public void resetRaindrop(int x)
+        {
+            this.Visible = false;
+            this.Location = new Point(x, -150);
+        }
+        /* private Rectangle kroplaPolozenie = new Rectangle();
+
+public ddropla() : base(Resources.kropla)
+{//to do owada
+    kroplaPolozenie.X = obrazX + 20;
+    kroplaPolozenie.Y = obrazY - 1;
+    kroplaPolozenie.Width = 127;
+    kroplaPolozenie.Height = 131;
+}
+
+public void Update(int x, int y)
+{//to do owada
+    obrazX = x;
+    obrazY = y;
+    kroplaPolozenie.X = obrazX + 20;
+    kroplaPolozenie.Y = obrazY - 1;
+}
+
+public bool Hit(int x, int y)
+{
+    Rectangle r = new Rectangle(x, y, 1, 1);
+    if (kroplaPolozenie.Contains(r))
+    {
+        return true;
+    }
+
+    return false;
+}*/
+    }
+}
